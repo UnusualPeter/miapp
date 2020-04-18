a22b0e97a3e7ef109aacca9613403a1c0c8e111a
UnusualPeter <peraltalopez@gmail.com>
2020-04-18 18:17:49 +0200

    Tutorial completo

--- miapp/urls.py
+++ miapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from miapp import views
 
+app_name = 'miapp'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('guardar', views.guardar, name='guardar'),
 ]
--- miapp/views.py
+++ miapp/views.py
@@ -1,5 +1,20 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.utils import timezone
+
+from miapp.models import Notas
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    notas = Notas.objects.order_by('pub_date').all()
+
+    return render(request, 'miapp/index.html', { 'notas': notas })
+
+def guardar(request):
+    texto = request.POST.get('texto')
+    fecha = timezone.now()
+
+    Notas.objects.create(texto=texto, pub_date=fecha)
+
+    return HttpResponseRedirect(reverse('miapp:home'))
